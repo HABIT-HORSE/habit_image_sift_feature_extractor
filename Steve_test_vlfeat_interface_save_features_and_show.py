c942fd5d114a72dd38e4b6c9505c63235824f3de
Steve North <Steve.North@nottingham.ac.uk>
2016-10-24 01:15:31 +0100

    Adding recursive search for keyframes in director

--- Steve_test_vlfeat_interface_save_features_and_show.py
+++ Steve_test_vlfeat_interface_save_features_and_show.py
@@ -2,12 +2,26 @@ from PIL import Image
 from pylab import *
 from vlfeat_interface import *
 
-#process_image('box.pgm','tmp.sift')
-process_image('test.jpg','tmp.sift')
-l,d = read_features_from_file('tmp.sift')
+keyframe_root_path = "./keyframes"
 
-im = array(Image.open('test.jpg'))
-figure()
-plot_features(im,l,True)
+'''
+Expected path to both input video keyframes and output SIFT feature files is: 
+./keyframes/behaviour_class_label/individual_video_directory/keyframes/
+'''
 
-show()
+for behaviour_class_label in os.listdir(keyframe_root_path):
+    print "\n"
+    path = keyframe_root_path + "/" + behaviour_class_label
+    print "behaviour_class_label: " + behaviour_class_label + "\n"
+	
+    for individual_video_directory in os.listdir(path):
+        #print behaviour_class +": "+ individual_video_directory
+        videoPath = path + "/" + individual_video_directory + "/keyframes/"
+        print "video: " + individual_video_directory
+		
+        for keyframe in os.listdir(videoPath):
+          if ".sift" in keyframe:
+            # skip directory called 'keyframes'...it's not a video!
+            continue
+          process_image(videoPath + keyframe,videoPath + keyframe + '.sift')
+        print "\n"
